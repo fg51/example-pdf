d7389593f7ec93e9b88e49f91a32b1ebd20eebac
kflange <zknsftk1msrk@gmail.com>
2021-04-30 13:42:39 +0900

    update

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,61 +1,33 @@
-use lopdf::content::{Content, Operation};
 use lopdf::dictionary;
-use lopdf::ObjectId;
-use lopdf::{Dictionary, Document, Object, Stream};
+use lopdf::{Object, Stream};
+
+use example_pdf as lib;
+
+use lib::{catalog, document, font, page, pages, resources, ContentBuilder};
 
 pub fn main() {
-    let mut doc = Document::with_version("1.5");
+    let font_family = "Courier";
+
+    let mut doc = document();
 
     let pages_id = doc.new_object_id();
-    let font_id = doc.add_object(font());
+    let font_id = doc.add_object(font(font_family));
     let resources_id = doc.add_object(resources(font_id));
-    let content = Content {
-        operations: vec![
-            Operation::new("BT", vec![]),
-            Operation::new("Tf", vec!["F1".into(), 48.into()]),
-            Operation::new("Td", vec![100.into(), 600.into()]),
-            Operation::new("Tj", vec![Object::string_literal("Hello World!")]),
-            Operation::new("ET", vec![]),
-        ],
-    };
 
+    let content = ContentBuilder::new()
+        .font("F1", 48)
+        .position((100, 600))
+        .text("good bye")
+        .build();
     let content_id =
-        doc.add_object(Stream::new(dictionary! {}, content.encode().unwrap()));
+        doc.add_object(Stream::new(dictionary! {}, content.encode().unwrap())); // Result
 
-    let page_id = doc.add_object(dictionary! {
-        "Type" => "Page",
-        "Parent" => pages_id,
-        "Contents" => content_id,
-    });
-    let pages = dictionary! {
-        "Type" => "Pages",
-        "Kids" => vec![page_id.into()],
-        "Count" => 1,
-        "Resources" => resources_id,
-        "MediaBox" => vec![0.into(), 0.into(),595.into(),842.into()],
-    };
+    let page_id = doc.add_object(page(pages_id, content_id));
+
+    let pages = pages(page_id, resources_id);
     doc.objects.insert(pages_id, Object::Dictionary(pages));
-    let catalog_id = doc.add_object(dictionary! {
-        "Type" => "Catalog",
-        "Pages" => pages_id,
-    });
+    let catalog_id = doc.add_object(catalog(pages_id));
     doc.trailer.set("Root", catalog_id);
     doc.compress();
-    doc.save("example.pdf").unwrap();
-}
-
-fn font() -> Dictionary {
-    dictionary! {
-        "Type" => "Font",
-        "Subtype" => "Type1",
-        "BaseFont" => "Courier",
-    }
-}
-
-fn resources(font_id: u32) -> Dictionary {
-    dictionary! {
-        "Font" => dictionary! {
-            "F1" => font_id,
-        },
-    }
+    doc.save("example.pdf").unwrap(); // Result
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,122 @@
+use lopdf::content::{Content, Operation};
+use lopdf::dictionary;
+use lopdf::ObjectId;
+use lopdf::{Dictionary, Document, Object};
+
+pub const PDF_VERSION: &'static str = "1.5";
+pub const BEGIN_TEXT: &'static str = "BT";
+pub const END_TEXT: &'static str = "ET";
+pub const TEXT_FONT: &'static str = "Tf";
+pub const TEXT_POSITION: &'static str = "Td";
+pub const TEXT_ONE: &'static str = "Tj";
+pub const TEXT_ANY: &'static str = "TJ";
+
+pub fn document() -> Document {
+    Document::with_version(PDF_VERSION)
+}
+
+pub fn font<'a>(base: &'a str) -> Dictionary {
+    dictionary! {
+        "Type" => "Font",
+        "Subtype" => "Type1",
+        "BaseFont" => base,
+    }
+}
+
+pub fn resources(font_id: ObjectId) -> Dictionary {
+    dictionary! {
+        "Font" => dictionary! {
+            "F1" => font_id,
+        },
+    }
+}
+
+pub fn page(pages_id: ObjectId, content_id: ObjectId) -> Dictionary {
+    dictionary! {
+        "Type" => "Page",
+        "Parent" => pages_id,
+        "Contents" => content_id,
+    }
+}
+
+pub fn pages(page_id: ObjectId, resources_id: ObjectId) -> Dictionary {
+    dictionary! {
+        "Type" => "Pages",
+        "Kids" => vec![page_id.into()],
+        "Count" => 1,
+        "Resources" => resources_id,
+        "MediaBox" => vec![0.into(), 0.into(),595.into(),842.into()],
+    }
+}
+
+pub fn catalog(pages_id: ObjectId) -> Dictionary {
+    dictionary! {
+        "Type" => "Catalog",
+        "Pages" => pages_id,
+    }
+}
+
+pub struct ContentBuilder<'a> {
+    font_resource: Option<&'a str>,
+    font_size: Option<u16>,
+    position: Option<(u32, u32)>,
+    texts: Vec<&'a str>,
+}
+
+impl<'a> ContentBuilder<'a> {
+    pub fn new() -> Self {
+        Self {
+            font_resource: None,
+            font_size: None,
+            position: None,
+            texts: vec![],
+        }
+    }
+
+    pub fn font(self, resource: &'a str, size: u16) -> Self {
+        Self {
+            font_resource: Some(resource),
+            font_size: Some(size),
+            ..self
+        }
+    }
+
+    pub fn position(self, position: (u32, u32)) -> Self {
+        Self {
+            position: Some(position),
+            ..self
+        }
+    }
+
+    pub fn text(mut self, text: &'a str) -> Self {
+        self.texts.push(text);
+        self
+    }
+
+    pub fn build(self) -> Content {
+        let position = self.position.unwrap();
+        let text = self.texts.join("");
+        Content {
+            operations: vec![
+                Operation::new(BEGIN_TEXT, vec![]),
+                Operation::new(
+                    TEXT_FONT,
+                    vec![
+                        self.font_resource.unwrap().into(),
+                        self.font_size.unwrap().into(),
+                    ],
+                ),
+                Operation::new(
+                    TEXT_POSITION,
+                    vec![position.0.into(), position.1.into()],
+                ),
+                Operation::new(TEXT_ONE, vec![Object::string_literal(text)]),
+                Operation::new(END_TEXT, vec![]),
+            ],
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
